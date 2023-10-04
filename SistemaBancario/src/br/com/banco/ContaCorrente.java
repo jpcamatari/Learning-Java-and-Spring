7ddb7748cc800aa817ea033728ced0627d6176ab
João Paulo Camatari <84422583+jpcamatari@users.noreply.github.com>
2023-10-04 00:19:51 -0300

    Add files via upload

---
+++ SistemaBancario/src/br/com/banco/ContaCorrente.java
@@ -0,0 +1,12 @@
+package br.com.banco;
+
+public class ContaCorrente extends Conta {
+
+    private double chequeEspecial();
+    public void setTipo(String tipo) {
+    }
+    public void setChequeEspecial(double chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+
+}
---
+++ SistemaBancario/src/br/com/banco/Teste.java
@@ -0,0 +1,14 @@
+package br.com.banco;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        ContaCorrente cc = new ContaCorrente();
+        cc.setAgencia(111);
+        cc.setNumero(321);
+        cc.setTipo("PF");
+        cc.setChequeEspecial(1000);
+
+    }
+}
